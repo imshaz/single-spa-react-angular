4411ca32f08d1dbf85d8ac630b602b8b197e82bf
imshaz <imshazbangash@gmail.com>
2021-07-29 17:35:19 +0500

    added side menu

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App({ name }) {
@@ -12,20 +12,27 @@ export default function App({ name }) {
             <li>
               <Link to="/react">Home</Link>
             </li>
+
+            <li>
+              <Link to="/angular/product-list">Product List</Link>
+            </li>
+            <li>
+              <Link to="/angular/add-product">Add product</Link>
+            </li>
             <li>
               <Link to="/react/Todos">Todos List</Link>
             </li>
             <li>
-              <Link to="/react/Add">Add Todo</Link>
+              <Link to="/react/addTodo">Add Todo</Link>
             </li>
           </ul>
         </nav>
         <div className="content">
           <Switch>
-            <Route path="/react/todos">
+            <Route exact path="/react/todos">
               <TodoList todos={todos} />
             </Route>
-            <Route path="/react/addTodo">
+            <Route exact path="/react/addTodo">
               <AddTodo />
             </Route>
             <Route path="/react">
@@ -43,12 +50,21 @@ function Home() {
 }
 
 function TodoList({ todos }) {
-  return <>
-    <h2>Todos</h2>
-    {todos.map()}
-  </>;
+  return (
+    <>
+      <h2>Todos</h2>
+      {/* {todos?.map()} */}
+    </>
+  );
 }
 
 function AddTodo() {
-  return <h2>Add Todo</h2>;
+  const [state, setState] = useState(false);
+  return (
+    <div>
+      <h2>Add Todo</h2>
+      state:{state ? "true" : "false"}
+      <button onClick={() => setState(!state)}>Toggle state</button>
+    </div>
+  );
 }
